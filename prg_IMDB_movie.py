49483f56d0ce7050d1bece2ef047db56478055ea
Sarthak Bhutani <isarthakbhutani@gmail.com>
2020-06-10 21:57:39 +0530

    second commit

--- prg_IMDB_movie.py
+++ prg_IMDB_movie.py
@@ -1,6 +1,9 @@
-print("Top rated IMDB movies")
+print("Check out this amazing Top 250 high rated Movies of all the time:\n")
+
 import imdb
 ia=imdb.IMDb()
 search=ia.get_top250_movies()
+count=1
 for i in range(250):
-    print(search[i])
+    print(f"{count}. {search[i]}")
+    count+=1
